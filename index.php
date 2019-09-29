f85270be5fef52a2cd32d164435b98fee5a84d8d
antonio <antoniocantu332@gmail.com>
2019-09-28 21:08:50 -0600

    commit 1 INICIO del proyecto agrimeint, index terminado

---
+++ index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Agrimeint</title>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans|PT+Sans:400,700" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/styles.css">
+        <link rel="stylesheet" href="assets/css/normalize.css">
+    </head>
+
+    <body>
+        <!-- site-header -->
+        <header class="site-header">
+            <div class="contenedor">
+                <!-- barra -->
+                <div class="barra">
+                    <a href="./index.php">
+                        <h1 class="no-margin">Agrimeint</h1>
+                    </a>
+                    <nav class="navegacion">
+                        <a href="nosotros.html">Nosotros</a>
+                        <a href="cursos.html">Publicaciones</a>
+                        <a href="contacto.html">Contacto</a>
+                        <a href="r">Registro</a>
+                    </nav>
+                </div>
+
+                <div class="texto-header">
+                    <h2 class="no-margin">De la mano amiga</h2>
+                    <p>somos una iniciativa que busca ayudar a los agricultores mexicanos</p>
+                    <a href="" class="boton boton-amarillo">Conoce más</a>
+                </div>
+            </div>
+        </header> 
+        <!-- fin site-header   -->
+
+        <section class="contenedor seccion">
+            <h2 class="fw-300 centrar-texto">Compra o vende sin intermediarios</h2>
+
+            <!-- iconos-bienvenida -->
+            <div class="iconos-bienvenida">
+                <div class="icono">
+                    <i class="fas fa-user-check"></i>
+                    <h3>Registrate gratis</h3>
+                    <p>Registrate gratis con tu cuenta de correo, en un solo click, facil y rapido</p>
+                </div>
+
+                <div class="icono">
+                    <i class="fas fa-tags"></i>
+                    <h3>Publica o compra</h3>
+                    <p>Publica o compra productos de agricultura, a buenos precios y varios productos</p>
+                </div>
+
+                <div class="icono">
+                    <i class="fas fa-mobile-alt"></i>
+                    <h3>Contacto directo</h3>
+                    <p>Contacta directamente a los usuarios, Sin intermediarios!</p>
+                </div>
+            </div>
+            <!-- iconos-bienvenida -->
+        </section>
+
+        <div class="seccion contenedor">
+            <h2 class="fw-300 centrar-texto">¡Algunos productos publicados!</h2>
+
+            <!-- contenedor-publicaciones -->
+            <div class="contenedor-publicaciones">
+                <!-- publicacion -->
+                <div class="publicacion">
+                    <img src="assets/img/agrimeint-logo.jpg" alt="Imagen producto">
+
+                    <!-- contenido-publicacion -->
+                    <div class="contenido-publicacion">
+                        <h3>Fresa recien cosechada</h3>
+                        <p><i class="fas fa-user"></i> Juan antonio</p>
+                        <p><i class="fas fa-map-marker-alt"></i> Mazatlan, sinaloa</p>
+                        <p>Fresa de calidad, riego con agua limpia disponible en tamaño y maduracion al gusto del cliente</p>
+                        <p class="precio"><i class="fas fa-tag"></i>$300</p>
+
+                        <a href="" class="boton boton-amarillo d-block">Ver Publicacion</a>
+                    </div>
+                    <!-- fin contenido-publicacion -->
+                </div>
+                <!-- fin publicacion -->
+
+                <div class="publicacion">
+                    <img src="assets/img/agrimeint-logo.jpg" alt="Imagen producto">
+
+                    <!-- contenido-publicacion -->
+                    <div class="contenido-publicacion">
+                        <h3>Fresa recien cosechada</h3>
+                        <p><i class="fas fa-user"></i> Juan antonio</p>
+                        <p><i class="fas fa-map-marker-alt"></i> Mazatlan, sinaloa</p>
+                        <p>Fresa de calidad, riego con agua limpia disponible en tamaño y maduracion al gusto del cliente</p>
+                        <p class="precio"><i class="fas fa-tag"></i>$300</p>
+
+                        <a href="" class="boton boton-amarillo d-block">Ver Publicacion</a>
+                    </div>
+                    <!-- fin contenido-publicacion -->
+                </div>
+
+                <div class="publicacion">
+                    <img src="assets/img/agrimeint-logo.jpg" alt="Imagen producto">
+
+                    <!-- contenido-publicacion -->
+                    <div class="contenido-publicacion">
+                        <h3>Fresa recien cosechada</h3>
+                        <p><i class="fas fa-user"></i> Juan antonio</p>
+                        <p><i class="fas fa-map-marker-alt"></i> Mazatlan, sinaloa</p>
+                        <p>Fresa de calidad, riego con agua limpia disponible en tamaño y maduracion al gusto del cliente</p>
+                        <p class="precio"><i class="fas fa-tag"></i>$300</p>
+
+                        <a href="" class="boton boton-amarillo d-block">Ver Publicacion</a>
+                    </div>
+                    <!-- fin contenido-publicacion -->
+                </div>
+
+                <div class="publicacion">
+                    <img src="assets/img/agrimeint-logo.jpg" alt="Imagen producto">
+
+                    <!-- contenido-publicacion -->
+                    <div class="contenido-publicacion">
+                        <h3>Fresa recien cosechada</h3>
+                        <p><i class="fas fa-user"></i> Juan antonio</p>
+                        <p><i class="fas fa-map-marker-alt"></i> Mazatlan, sinaloa</p>
+                        <p>Fresa de calidad, riego con agua limpia disponible en tamaño y maduracion al gusto del cliente</p>
+                        <p class="precio"><i class="fas fa-tag"></i>$300</p>
+
+                        <a href="" class="boton boton-amarillo d-block">Ver Publicacion</a>
+                    </div>
+                    <!-- fin contenido-publicacion -->
+                </div>
+
+                <div class="publicacion">
+                    <img src="assets/img/agrimeint-logo.jpg" alt="Imagen producto">
+
+                    <!-- contenido-publicacion -->
+                    <div class="contenido-publicacion">
+                        <h3>Fresa recien cosechada</h3>
+                        <p><i class="fas fa-user"></i> Juan antonio</p>
+                        <p><i class="fas fa-map-marker-alt"></i> Mazatlan, sinaloa</p>
+                        <p>Fresa de calidad, riego con agua limpia disponible en tamaño y maduracion al gusto del cliente</p>
+                        <p class="precio"><i class="fas fa-tag"></i>$300</p>
+
+                        <a href="" class="boton boton-amarillo d-block">Ver Publicacion</a>
+                    </div>
+                    <!-- fin contenido-publicacion -->
+                </div>
+
+                <div class="publicacion">
+                    <img src="assets/img/agrimeint-logo.jpg" alt="Imagen producto">
+
+                    <!-- contenido-publicacion -->
+                    <div class="contenido-publicacion">
+                        <h3>Fresa recien cosechada</h3>
+                        <p><i class="fas fa-user"></i> Juan antonio</p>
+                        <p><i class="fas fa-map-marker-alt"></i> Mazatlan, sinaloa</p>
+                        <p>Fresa de calidad, riego con agua limpia disponible en tamaño y maduracion al gusto del cliente</p>
+                        <p class="precio"><i class="fas fa-tag"></i>$300</p>
+
+                        <a href="" class="boton boton-amarillo d-block">Ver Publicacion</a>
+                    </div>
+                    <!-- fin contenido-publicacion -->
+                </div>
+
+            </div>
+            <!-- fin contenedor-publicaciones -->
+
+            <div class="ver-todas">
+                <a href="#" class="boton boton-verde">Ver todas las publicaciones</a>
+            </div>
+        </div>
+
+        <!-- site-footer -->
+        <footer class="site-footer seccion">
+            <!-- contenidor-footer -->
+            <div class="contenido-footer contenedor">
+                <nav class="navegacion">
+                    <a href="nosotros.html">Nosotros</a>
+                    <a href="anuncios.html">Publicaciones</a>
+                    <a href="blog.html">Contacto</a>
+                    <a href="contacto.html">Registro</a>
+                </nav>
+                <p class="copyright">Todos los Derechos Reservados 2019 &copy Agrimeint </p>
+            </div>
+        </footer>
+        <!-- fin site-footer -->
+
+    </body>
+</html>
